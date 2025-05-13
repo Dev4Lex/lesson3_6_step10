c363f285db4c430b146123f85a56b818e81e8a6f
Alex <dev4lex@gmail.com>
2025-05-13 15:48:13 +0300

    add test_items.py and confest.py

---
+++ test_items.py
@@ -0,0 +1,19 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class TestBasketButton:
+    def test_add_to_basket_button_is_present(self, browser):
+        browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+        time.sleep(30)
+        add_to_basket_button = WebDriverWait(browser, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket")))
+        assert add_to_basket_button.is_displayed()
+
+
+if __name__ == "__main__":
+    pytest.main()
