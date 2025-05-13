3e85f22624ea2cdd81c71e464b59783e905fb822
Alex <dev4lex@gmail.com>
2025-05-13 15:49:55 +0300

    delete timesleep

--- test_items.py
+++ test_items.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 class TestBasketButton:
     def test_add_to_basket_button_is_present(self, browser):
         browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-        time.sleep(30)
         add_to_basket_button = WebDriverWait(browser, 15).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket")))
         assert add_to_basket_button.is_displayed()
